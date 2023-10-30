f0027248e7ee2d87e84f313de01bb7a1be4c9301
Chinonso Chukwuogor <nonsochukwuogor@gmail.com>
2023-10-30 16:13:19 -0700

    Promote to AIO repo (#12)
    Promote to AIO repo
    
    Co-authored-by: Azure IoT Operations Bot <actionbot@azure.com>

--- tools/cleanup-cluster/components/extension.sh
+++ tools/cleanup-cluster/components/extension.sh
@@ -9,25 +9,36 @@ echo "Deleting Arc extensions..."
 
 # AIO
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n azure-iot-operations --yes --force
+
 # Symphony
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n alice-springs --yes --force
+
 # Akri
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n akri --yes --force
+
 # Bluefin
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n processor --yes --force
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n bluefin --yes --force
+
 # E4K
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n data-plane --yes --force
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n e4k --yes --force
+
 # MQ
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n mq --yes --force
+
 # OPCUA
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n opc-ua-broker --yes --force
+
 # ADR
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n assets --yes --force
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n adr --yes --force
+
 # Secret
 az k8s-extension delete -g $1 -c $2 -t connectedClusters -n akvsecretsprovider --yes --force
+
+# Network
+az k8s-extension delete -g $1 -c $2 -t connectedClusters -n layered-networking --yes --force
 
 echo ""
 az k8s-extension list -g $1 -c $2 -t connectedClusters -o table
