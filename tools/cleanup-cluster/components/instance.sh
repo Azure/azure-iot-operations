4cd0c24d2211d015bae943bfd26cc7bc4a3f5621
chgennar <56695749+chgennar@users.noreply.github.com>
2024-10-02 21:23:12 +0000

    AIO 0.7.31 Release

--- tools/cleanup-cluster/components/instance.sh
+++
@@ -1,9 +0,0 @@
-echo ""
-echo "##############################################################"
-echo "I'm instance.sh"
-
-# Clean up finalizers to allow deletion of instances
-kubectl -n azure-iot-operations get -o name instances.orchestrator.iotoperations.azure.com | xargs -I {} kubectl -n azure-iot-operations patch {} -p '{"metadata":{"finalizers":[]}}' --type=merge
-
-echo ""
-kubectl get instances.orchestrator.iotoperations.azure.com -A
